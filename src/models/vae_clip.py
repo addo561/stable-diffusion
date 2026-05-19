8f0f9e55bcff69627d5d129ed8ee448729b58068
addo561 <korlilarryaddo43@gmail.com>
2026-05-19 11:23:48 +0000

    [Done] autoencoder / clip  class module

--- src/models/vae_clip.py
+++ src/models/vae_clip.py
@@ -5,8 +5,11 @@ import torch.nn  as nn
   
 class Clip_VAE(nn.Module):
     def __init__(self,
-                 model_name:  str,
-                 device
+                model_name:  str,
+                device,
+                tokenizer = None,
+                text_encoder = None,
+                Vae = None 
                  ):
         """Module for clip tokenizer,text_encoder and vae decoder/encoder
 
@@ -17,17 +20,21 @@ class Clip_VAE(nn.Module):
         super().__init__()
         self.model_name =  model_name
         self.device = device
+        self.tokenizer  = tokenizer
+        self.text_encoder = text_encoder
+        self.Vae = Vae
+        
         if self.model_name =='clip':
-            self.tokenizer = CLIPTokenizer.from_pretrained('CompVis/stable-diffusion-v1-4',subfolder='tokenizer')
-            self.text_encoder = CLIPTextModel.from_pretrained('CompVis/stable-diffusion-v1-4',subfolder='text_encoder').to(self.device)
+            self.get_tokens = self.tokenizer.from_pretrained('CompVis/stable-diffusion-v1-4',subfolder='tokenizer')
+            self.get_embeddings = self.text_encoder.from_pretrained('CompVis/stable-diffusion-v1-4',subfolder='text_encoder').to(self.device)
             
-        if  self.model_name == 'Vae_encode':
-            pass
-        if self.model_name == 'Vae_decode':
-            pass    
+        if  self.model_name == 'Vae_encode' or self.model_name == 'Vae_decode':
+            self.latent_scaling_factor =  0.18215
+            self.autoencoder = self.Vae.from_pretrained('CompVis/stable-diffusion-v1-4',subfolder='vae')
+            
     def forward(self,input : str | torch.Tensor)-> torch.Tensor:
         if self.model_name=='clip':
-            input_ids = self.tokenizer([input],
+            input_ids = self.get_tokens([input],
                                        paddings='max_length',
                                        max_length = self.tokenizer.model_max_length,
                                        truncation=True, 
@@ -35,6 +42,11 @@ class Clip_VAE(nn.Module):
                                        )['input_ids'] 
             input_ids_tensor =  input_ids.to(self.device)  
             with torch.no_grad():
-                text_embeddings =   self.text_encoder(input_ids_tensor)['last_hidden_state']
-            return  text_embeddings.shape
-            
+                text_embeddings =   self.get_embeddings(input_ids_tensor)['last_hidden_state']
+            return  text_embeddings
+        if  self.model_name == 'Vae_encode':
+            encoded = self.latent_scaling_factor * self.autoencoder.encode(input).sample()
+            return encoded
+        if  self.model_name == 'Vae_decode':
+            decoded  = self.autoencoder.decode(input/self.latent_scaling_factor).sample()
+            return decoded
