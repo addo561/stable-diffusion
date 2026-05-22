1ce79c742e80fc6bfee6766fc1e9a930a53dc9b8
addo561 <korlilarryaddo43@gmail.com>
2026-05-22 09:36:56 +0000

    made decoder  changes

--- src/models/vae_clip.py
+++ src/models/vae_clip.py
@@ -32,26 +32,22 @@ class Clip_VAE(nn.Module):
         if  self.model_name == 'Vae_encode' or self.model_name == 'Vae_decode':
             self.latent_scaling_factor =  0.18215
             self.autoencoder = self.Vae.from_pretrained('CompVis/stable-diffusion-v1-4',subfolder='vae').to(device)
-            
+    @torch.inference_mode()        
     def forward(self,input : str | torch.Tensor)-> torch.Tensor:
         if self.model_name=='clip':
             input_ids = self.get_tokens([input],
-                                       paddings='max_length',
-                                       max_length = self.tokenizer.model_max_length,
+                                       padding ='max_length',
+                                       max_length = 77, #hardcoded as 77
                                        truncation=True, 
                                        return_tensors="pt",
                                        )['input_ids'] 
             input_ids_tensor =  input_ids.to(self.device)  
-            with torch.no_grad():
-                text_embeddings =   self.get_embeddings(input_ids_tensor)['last_hidden_state']
+            text_embeddings =   self.get_embeddings(input_ids_tensor)['last_hidden_state']
             return  text_embeddings
         input = input.to(self.device)
         if  self.model_name == 'Vae_encode':
-            with torch.no_grad():
-                encoded = self.latent_scaling_factor * self.autoencoder.encode(input).sample()
+            encoded = self.latent_scaling_factor * self.autoencoder.encode(input).sample()
             return encoded
         if  self.model_name == 'Vae_decode':
-            with torch.no_grad():
-                decoded  = self.autoencoder.decode(input/self.latent_scaling_factor).sample()
-                
+            decoded  = self.autoencoder.decode(input/self.latent_scaling_factor).sample()
             return (decoded/2 + 0.5).clamp(0,1) # [-1,1] to [0,1]
